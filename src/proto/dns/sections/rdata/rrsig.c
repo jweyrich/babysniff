f4647376318cba4380c5208bcc200176622011bc
Jardel Weyrich <jweyrich@gmail.com>
2025-09-14 01:39:24 -0300

    dns: Fix signature parsing for RR of type RRSIG

--- src/proto/dns/sections/rdata/rrsig.c
+++ src/proto/dns/sections/rdata/rrsig.c
@@ -4,29 +4,13 @@
 #include "proto/dns/name.h"
 #include "proto/dns/sections/rdata.h"
 #include "proto/dns/arrays.h"
-#include "base64.h"
-#include <stdlib.h> // for malloc
-#include <string.h> // for strlen
+#include "reader.h"
+#include <stdlib.h>
 #include <netinet/in.h> // for ntohs and ntohl
 #include <time.h> // for gmtime_r + strftime
 
-static char *parse_signature(buffer_t *buffer) {
-	//pd3IpdiZHH7ig7szxDcWSKtkmpK52w7hcCJs/6TL74AH2Wyd4N4pAYbpRuNSuuPHwH+fT8P9f+TqKTeTa2DSzD1bumgICnHhOi9yO0pAYdsyThFdiiJtg8vPMyyMagxhJkurienCAVA4nqF40cMwJgAfHS+Vc+EQSlbDVFjmI5s=
-	//M+5jGt0Xd5+fnvfyCNG7v7quzJ6p5sABjWVz0L/kUyN0erX4eNzpzFiofiRFnYkwAaibP+GcW/kB/ibots9e4sPhHvPWZs/01kgVgust9VN7nOiPON8dMkCJPPOrsz1SfcDqBj3ES5wNT/C2bTle4QOgv9z9XKdcNtlyeWmBW0h9Co+SMutYHpHoiBCKhcgI
-	// TODO(jweyrich): What's the real size? 128 or 144? Is it constant?
-	const size_t input_size = 128; // RSA/SHA-1
-	uint8_t signature[input_size];
-	int read = buffer_read(buffer, signature, input_size);
-	if (read == 0)
-		return NULL;
-	// Base64
-	const size_t encoded_size = base64_encoded_size(input_size);
-	char *encoded = malloc(encoded_size + 1);
-	if (encoded == NULL)
-		return NULL;
-	base64_encode(encoded, encoded_size, signature, input_size);
-	LOG_DEBUG("input_size = %zu, encoded_size = %zu, result_size = %zu\n", input_size, encoded_size, strlen(encoded));
-	return encoded;
+char *read_signature(buffer_t *from_buffer, int *error, size_t size) {
+	return read_bytes_and_base64(from_buffer, error, size);
 }
 
 int parse_rdata_rrsig(dns_rdata_t *rdata, buffer_t *buffer) {
@@ -46,12 +30,20 @@ int parse_rdata_rrsig(dns_rdata_t *rdata, buffer_t *buffer) {
 		LOG_WARN("RRSIG signer name is NULL");
 		return -1;
 	}
-	// FIXME(jweyrich): parse_signature is not working properly.
-	// rdata->rrsig.signature = parse_signature(buffer);
-	// if (rdata->rrsig.signature == NULL) {
-	// 	LOG_WARN("RRSIG signature is NULL");
-	// 	return -1;
-	// }
+
+	// TODO(jweyrich): Figure out sizes depending on algorithm
+	size_t signature_size = 64;
+	int read_error = 0;
+	rdata->rrsig.signature = read_signature(buffer, &read_error, signature_size);
+	if (read_error != 0) {
+		LOG_WARN("error while reading RRSIG signature: %d", read_error);
+		return -1;
+	}
+	if (buffer_has_error(buffer)) {
+		LOG_WARN("detected an error in the buffer while reading RR of type RRSIG");
+		return -1;
+	}
+
 	rdata->rrsig.typec = ntohs(rdata->rrsig.typec);
 	rdata->rrsig.original_ttl = ntohl(rdata->rrsig.original_ttl);
 	rdata->rrsig.signature_expiration = ntohl(rdata->rrsig.signature_expiration);
