bb070e537ec36d5ad038bce6bfbd1b12237da76b
Jardel Weyrich <jweyrich@gmail.com>
2025-10-12 01:45:18 -0300

    Make signal handlers work on Windows

--- src/babysniff.c
+++ src/babysniff.c
@@ -4,22 +4,7 @@
 #	define __USE_POSIX
 #endif
 
-// #ifndef _GNU_SOURCE
-// #define _POSIX_C_SOURCE 2 // POSIX.1, POSIX.2
-// #define _SVID_SOURCE // SVID, ISO C, POSIX.1, POSIX.2, X/Open
-// #define _BSD_SOURCE 1 // 4.3 BSD Unix, ISO C, POSIX.1, POSIX.2
-// #endif
-
-// #if _POSIX_C_SOURCE >= 1 || defined(_XOPEN_SOURCE) || defined(_POSIX_SOURCE)
-// #define HAVE_SIGACTION // sigaction is part of POSIX.1
-// #endif
-
-// #ifndef HAVE_SIGACTION
-// #error sigaction is not supported
-// #endif
-
 #include <fcntl.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -29,26 +14,8 @@
 #include "config.h"
 #include "daemon.h"
 #include "security.h"
-
-// sig_atomic_t is defined by C99
-static volatile sig_atomic_t g_done = 0;
-
-static void cleanup(int signal) {
-	printf("Received signal %d\n", signal);
-	if (signal == SIGINT)
-		g_done = 1;
-}
-
-static void install_sighandlers(void) {
-	struct sigaction sa;
-	sa.sa_handler = &cleanup;
-	sa.sa_flags = 0;
-	sigemptyset(&sa.sa_mask);
-	sigaction(SIGHUP, &sa, NULL);
-	sigaction(SIGINT, &sa, NULL);
-	sigaction(SIGTERM, &sa, NULL);
-	sigaction(SIGQUIT, &sa, NULL);
-}
+#include "signal_handler.h"
+#include "system.h"
 
 int main(int argc, char **argv) {
 	cli_args_t args;
@@ -79,7 +46,10 @@ int main(int argc, char **argv) {
 	if (args.background)
 		daemonize(&args);
 
-	install_sighandlers();
+	if (signal_handler_init() < 0) {
+		fprintf(stderr, "Failed to initialize signal handlers\n");
+		return EXIT_FAILURE;
+	}
 
 	if (args.background) {
 		// Redirect std{in|err|out} to /dev/null
@@ -123,9 +93,11 @@ int main(int argc, char **argv) {
 			goto error;
 	}
 
-	while (!g_done) {
+	while (!signal_handler_is_interrupted()) {
 		sniff_readloop(channel, 1, &config);
 	}
+
+	signal_handler_cleanup();
 	sniff_close(channel);
 
 	printf("Terminating...\n");
